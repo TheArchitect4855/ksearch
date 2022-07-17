ecf0e6bd14d16ebc93b5134f04ba0dcbdb88eb3f
Kurtis Knodel <kurtisknodel@gmail.com>
2022-07-17 12:48:51 -0700

    Indexes can now be added to and don't need to be reset

--- src/index.rs
+++ src/index.rs
@@ -1,7 +1,8 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fmt::Display;
-use std::io::{Write, Read};
+use std::io::{Write, Read, Seek, SeekFrom};
+use std::path::Path;
 use std::{collections::HashSet, path::PathBuf};
 use std::time;
 use std::fs;
@@ -59,14 +60,70 @@ pub fn create_indices(url: &str, tags: &HashSet<String>) -> Result<u64, crate::E
 
 pub fn create_index(page_id: u64, tag: &str) {
 	let indices = get_tag_index(tag);
+	let exists = indices.exists();
 	let mut file = fs::File::options()
 		.append(true)
 		.create(true)
-		.open(indices)
+		.read(true)
+		.open(&indices)
 		.expect("Failed to open output file");
 	
-	let buf = page_id.to_be_bytes();
-	file.write_all(&buf).expect("Failed to write to output file");
+	let mut write_to = 0;
+	let mut file_size = 0;
+	if exists {
+		let meta = file.metadata().expect("Failed to get file metadata");
+		file_size = meta.len();
+		let num_indices = file_size / 8;
+
+		let mut seek_to = num_indices / 2;
+		let mut partition = (0, num_indices);
+		let mut buffer = [0; 8];
+		write_to = loop {
+			file.seek(SeekFrom::Start(seek_to * 8)).expect("Failed to seek file");
+			file.read_exact(&mut buffer).expect("Failed to read from file");
+			
+			let pid = u64::from_be_bytes(buffer);
+			if pid > page_id && partition.1 != seek_to {
+				// Seek back
+				partition.1 = seek_to;
+			} else if pid < page_id && partition.0 != seek_to {
+				// Seek forwards
+				partition.0 = seek_to;
+			} else if pid == page_id {
+				// If we find our page ID, then this is already indexed
+				return;
+			}
+
+			if partition.0 + 1 == partition.1 {
+				break partition.1 * 8;
+			}
+
+			let min = partition.1 - partition.0;
+			seek_to = (min / 2) + partition.0;
+		};
+	}
+
+	if file_size == 0 {
+		let buf = page_id.to_be_bytes();
+		file.write_all(&buf).expect("Failed to write index");
+		return;
+	}
+
+	file.seek(SeekFrom::Start(write_to)).expect("Failed to seek file");
+	let mut ahead = [0; 8];
+	let mut write = page_id.to_be_bytes();
+	while write_to < file_size {
+		file.read_exact(&mut ahead).expect("Failed to read index");
+		file.seek(SeekFrom::Current(-8)).expect("Failed to seek file");
+		file.write_all(&write).expect("Failed to write index");
+		write = ahead;
+		write_to += 8;
+	}
+
+	file.write_all(&write).expect("Failed to write index");
+	if cfg!(debug_assertions) {
+		validate_index(&indices);
+	}
 }
 
 pub fn query(tags: &HashSet<String>) -> Box<[QueryResult]> {
@@ -143,6 +200,20 @@ pub fn query(tags: &HashSet<String>) -> Box<[QueryResult]> {
 		rank: *page_ranks.get(&v.0).unwrap(),
 		hits: *tag_counts.get(&v.0).unwrap(),
 	}).collect::<Vec<QueryResult>>().into_boxed_slice()
+}
+
+pub fn validate_index(path: &Path) {
+	let mut file = fs::File::open(path).expect("Failed to open index");
+	let mut last = 0;
+	let mut buf = [0; 8];
+	while let Ok(_) = file.read_exact(&mut buf) {
+		let n = u64::from_be_bytes(buf);
+		if last >= n {
+			panic!("Index validation failed");
+		}
+
+		last = n;
+	}
 }
 
 fn get_tag_index(tag: &str) -> PathBuf {
